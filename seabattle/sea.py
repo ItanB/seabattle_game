330f4da72ccabd54f71c77baf644cb548c6ee130
Igor <bid77763@gmail.com>
2023-05-18 23:20:32 +0300

    Init Skillfactory

---
+++ seabattle/sea.py
@@ -0,0 +1,51 @@
+import random
+
+board_size = 5
+board = []
+
+for x in range(board_size):
+    board.append(["O"] * board_size)
+
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+
+print("Let's play Sea Battle!")
+print_board(board)
+
+
+def random_row(board):
+    return random.randint(0, len(board) - 1)
+
+
+def random_col(board):
+    return random.randint(0, len(board[0]) - 1)
+
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(4):
+    print("Turn", turn + 1)
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sank my battleship!")
+        break
+    else:
+        if guess_row not in range(board_size) or \
+                guess_col not in range(board_size):
+            print("Oops, that's not even in the ocean.")
+        elif (board[guess_row][guess_col] == "X"):
+            print("You guessed that one already.")
+        else:
+            print("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+
+        if (turn == 3):
+            print("Game Over")
+
+        print_board(board)
